d1168696175ef44b6e169ee29c3d153ee0a65be2
haytam-lahna <haytamleo10@gmail.com>
2025-09-14 16:16:24 +0100

    Add files via upload

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const firstArg = process.argv[2];
+const x = parseInt(firstArg);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
